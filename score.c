7711b5f6fb3f4ae677a3f09599b8ea8f8f19cafd
Reage-X <sergekshyvnyak@gmail>
2024-11-14 12:16:49 +0100

    test

---
+++ score.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int score(int O, int +) {
+    if(O==1)
+    {
+        int nx = 0, ny = 0;
+        int x = 2, y = 10;
+        if(+==1)
+        {
+            y= y +10;
+        }
+        int monTableau[x][y];
+        for (ny = 0; ny < y; ny++)
+        {
+            nx = 0;
+            \\
+            appelle pseudo
+            \\
+            nx++;
+            \\
+            appelle score
+            \\
+            nx = 0;
+        }
+        for (nx = 0; nx < x; nx++)
+        {
+            printf("\n");
+            for (ny = 0; ny < y; ny++)
+            {
+                printf("%d  ", monTableau[nx][ny]);
+            }
+        }
+        printf("\n");}
+    }
+    return 0;
+}
