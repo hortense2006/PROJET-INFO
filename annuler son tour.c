5a062c60bf831d8cbd9a15e929a22ba50e86ff89
hortense2006 <hortense.galtier@edu.ece.fr>
2024-11-15 20:13:59 +0100

    d

--- annuler son tour.c
+++ annuler son tour.c
@@ -1,17 +1,51 @@
 //
 // Created by galti on 14/11/2024.
 //
+#include <stdio.h>
+#include <options.h>
+#define MIN 2
+#define MAX 4
 int annuler(){
-
-  // DEFINITION DES VARIABLES
+  // DECLARATION DES VARIABLES
+  int nbjoueur;
   int joueur;
-  int tour;
-  int j;
+  int tour_du_joueur;
+  int tour_precedent;
+  int chiffrei;
+  int i;
+  printf("Voulez-vous annuler votre tour precedent?\n");
+  printf("1. Oui\n");
+  printf("2. Non\n");
+  switch (chiffrei){
+    case 1:{
+      // ON VERIFIE QUE LE JOUEUR N'A PAS DEJA ANNULE UN TOUR
+      if (tour_du_joueur != tour_precedent ){
+        printf("Le joueur n'a pas encore annule un tour.\n");
+      }
+      else{
+        printf("Le joueur a deja annule un tour.\n");
+        options();
+      }
+      // LE JOUEUR DECIDE D'ANNULER SON TOUR
+      if (tour_du_joueur == '\n'){
+        tour_du_joueur = tour_precedent;
+      }
+      else{
+        printf("Le joueur a decide de jouer.\n");
+      }
+      break;
+    }
+     case 2:{
+       options();
+       break;
+     }
+       default:{
+         printf("Erreur!\n");
+         break;
+       }
+  }
 
-  // LE JOUEUR ANNULE SON TOUR
-  // UNE SEULE ANNULATION POSSIBLE
-
-
+  }
   return 0;
 }
 #include "annuler son tour.h"
