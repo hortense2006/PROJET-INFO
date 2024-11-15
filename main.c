3cf33a61ad2dfef2395d6ddb23bea948e6a64677
hortense2006 <hortense.galtier@edu.ece.fr>
2024-11-15 18:13:42 +0100

    d

--- main.c
+++
@@ -1,8 +0,0 @@
-//
-// Created by galti on 15/11/2024.
-//
-int main(void){
-  int accueil();
-  int menu();
-
-#include "main.h"
--- main.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by galti on 15/11/2024.
-//
-
-#ifndef MAIN_H
-#define MAIN_H
-
-#endif //MAIN_H
