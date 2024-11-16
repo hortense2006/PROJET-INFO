c00e4557ad5ebe6ad3ba9d9346e6f2292d99c5c4
hortense2006 <hortense.galtier@edu.ece.fr>
2024-11-16 18:52:46 +0100

    d

--- déplacement du pion.c
+++ déplacement du pion.c
@@ -13,6 +13,7 @@ int pion() {
     int chiffre2;
     int position_initiale_x;
     int position_initiale_y;
+    int possible;
     // POSITION INITIALE DU PION
     if (nbjoueur ==2){
       void gotoligcol(int lig2,int col2){
@@ -27,18 +28,18 @@ int pion() {
     /* PRESENTATION DES OPTIONS*/
     printf("Vous pouvez deplacer votre pion de 4 manieres :\n");
 
-    printf("1 = Aller vers la gauche.\n",gauche);
-    printf("2 = Aller vers la droite\n",droite);
-    printf("3 = Avancer d'une case.\n",devant);
-    printf("4 = Reculer d'une case.\n",derriere);
+    printf("1 = Aller vers la gauche.\n");
+    printf("2 = Aller vers la droite\n");
+    printf("3 = Avancer d'une case.\n");
+    printf("4 = Reculer d'une case.\n");
 
     /* CHOIX D'UN CHIFFRE RELIE A UNE OPTION DE DEPLACEMENT*/
 
-    printf("Selectionner un chiffre entre 1 et 4:",chiffre2);
+    printf("Selectionner un chiffre entre 1 et 4:");
     scanf("%d",&chiffre2);
     switch (chiffre2) {
         case 1: {
-            printf("Voulez-vous aller d'une case vers la gauche ?\n",gauche);
+            printf("Voulez-vous aller d'une case vers la gauche ?\n");
             printf("1 = Oui\n");
             printf("2 = Non\n");
             printf("Selectionner soit 1 soit 2:\n");
@@ -118,8 +119,33 @@ int pion() {
                 /* VERIFIER SI LE MOUVEMENT EST POSSIBLE*/
                 if (SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),mycoord)!='\n') {
                   printf("Cette case est déjà occupée.\n");
-                  printf("Faites un autre choix.\n");
-                  pion();
+                  /* PASSER AU DESSUS SI C'EST UN PION*/
+                  if (SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),mycoord)!='\n') {
+                    printf("Voulez-vous passer au dessus du pion ?\n");
+                    printf("1 = Oui\n");
+                    printf("2 = Non\n");
+                    printf("Selectionner soit 1 soit 2:\n");
+                    scanf("%d",&possible);
+                    switch (possible) {
+                      case 1: {
+                        /*PASSER AU DESSUS DU PION*/
+                        break;
+                      }
+                      case 2: {
+                        pion();
+                        break;
+                      }
+                      default:{
+                        printf("Erreur!\n")
+                        pion();
+                        break;
+                      }
+                    }
+                    /* SINON C'EST UNE BARRIERE*/
+                  else{
+                    printf("Cette case est déjà occupee par une barriere. Faites un autre choix.\n");
+                    pion();
+                  }
                 else{
                   printf("Votre choix est enregistre.\n");
                 }
