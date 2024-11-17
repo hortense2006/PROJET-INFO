ad51678cd1eb1684df1f30644eef272aad990e4a
hortense2006 <hortense.galtier@edu.ece.fr>
2024-11-17 18:15:51 +0100

    d

--- aide.c
+++ aide.c
@@ -14,11 +14,11 @@ int aide(){
   printf("\n");
   printf("Regle 4: Si votre pion se retrouve face à face avec un autre pion, vous pouvez passer au dessus de celui-ci.\n");
   printf("\n");
-  printf("Regle 4: Si votre partie est a 4 joueurs, vous ne pouvez pas sauter plus d'un pion a la fois.\n");
+  printf("Regle 5: Si votre partie est a 4 joueurs, vous ne pouvez pas sauter plus d'un pion a la fois.\n");
   printf("\n");
-  printf("Regle 5: Vous ne pouvez annuler votre tour precedent qu'une seule fois\n");
+  printf("Regle 6: Vous ne pouvez annuler votre tour precedent qu'une seule fois\n");
   printf("\n");
-  printf("Regle 6: Vous pouvez acceder a votre score via le menu. Celui-ci est automatiquement sauvegarde.\n");
+  printf("Regle 7: Vous pouvez acceder a votre score via le menu. Celui-ci est automatiquement sauvegarde.\n");
   return 0;
   }
 
