4c3540ae670f83cfb25da6eaf709e575e94146cd
hortense2006 <hortense.galtier@edu.ece.fr>
2024-11-17 10:43:00 +0100

    d

--- passer au dessus du pion.c
+++ passer au dessus du pion.c
@@ -6,32 +6,33 @@
 int au_dessus_du_pion(){
     /* PASSER AU DESSUS SI C'EST UN PION*/
     if (SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),mycoord)!='\n') {
-        printf("Voulez-vous passer au dessus du pion ?\n");
-        printf("1 = Oui\n");
-        printf("2 = Non\n");
-        printf("Selectionner soit 1 soit 2:\n");
-        scanf("%d",&possible);
-        switch (possible) {
-            case 1: {
-                /*PASSER AU DESSUS DU PION*/
-                break;
-            }
-            case 2: {
-                pion();
-                break;
-            }
-            default:{
-                printf("Erreur!\n")
-                pion();
-                break;
-            }
-
-        }
-        /* SINON C'EST UNE BARRIERE*/
-        else{
-            printf("Cette case est déjà occupee par une barriere. Faites un autre choix.\n");
-            pion();
-        }
+      if (SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),mycoord)== /*PION*/) {
+          printf("Voulez-vous passer au dessus du pion ?\n");
+          printf("1 = Oui\n");
+          printf("2 = Non\n");
+          printf("Selectionner soit 1 soit 2:\n");
+          scanf("%d",&possible);
+          switch (possible) {
+              case 1: {
+                  /*PASSER AU DESSUS DU PION*/
+                  break;
+              }
+              case 2: {
+                  pion();
+                  break;
+              }
+              default:{
+                  printf("Erreur!\n")
+                  pion();
+                  break;
+              }
+          }
+      }
+      else {
+          /* SINON C'EST UNE BARRIERE*/
+          printf("Cette case est déjà occupee par une barriere. Faites un autre choix.\n");
+          pion();
+      }
     }
     return 0;
 }
