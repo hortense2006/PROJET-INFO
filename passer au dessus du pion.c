6aa352b84fff5b88e5793f0afad4b360d331fb7b
Reage-X <sergekshyvnyak@gmail>
2024-11-17 16:41:40 +0100

    test

--- passer au dessus du pion.c
+++ passer au dessus du pion.c
@@ -5,38 +5,10 @@
 #include <windows.h>
 #include "déplacement du pion.h"
 int au_dessus_du_pion(){
-    int possible;
-    /* PASSER AU DESSUS SI C'EST UN PION*/
-    if (SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),mycoord)!='\n') {
-      if (SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),mycoord)== /*PION*/) {
-          printf("Voulez-vous passer au dessus du pion ?\n");
-          printf("1 = Oui\n");
-          printf("2 = Non\n");
-          printf("Selectionner soit 1 soit 2:\n");
-          scanf("%d",&possible);
-          switch (possible) {
-              case 1: {
-                  /*PASSER AU DESSUS DU PION*/
-                  break;
-              }
-              case 2: {
-                  pion();
-                  break;
-              }
-              default:{
-                  printf("Erreur!\n");
-                  pion();
-                  break;
-              }
-          }
-      }
-      else {
-          /* SINON C'EST UNE BARRIERE*/
-          printf("Cette case est déjà occupee par une barriere. Faites un autre choix.\n");
-          pion();
-      }
-    }
-    return 0;
-}
+     îf(yposj(x)=yposj(y) && (xposj(x)=xposj(y)-1||xposj(x)=xposj(y)+1)
+     // axe de mvt souhaiter +2
+
+     îf(xposj(x)=xposj(y) &&(yposj(x)=yposj(y)-1||yposj(x)=yposj(y)+1)
+     // axe de mvt souhaiter +2
 
 #include "passer au dessus du pion.h"
