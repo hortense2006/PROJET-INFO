4f120ef9e0dd9701341abd34a06fa85baecab5ba
hortense2006 <hortense.galtier@edu.ece.fr>
2024-11-17 16:56:57 +0100

    d

--- passer au dessus du pion.c
+++ passer au dessus du pion.c
@@ -3,12 +3,15 @@
 //
 #include <stdio.h>
 #include <windows.h>
-#include "déplacement du pion.h"
-int au_dessus_du_pion(){
-     îf(yposj(x)=yposj(y) && (xposj(x)=xposj(y)-1||xposj(x)=xposj(y)+1)
-     // axe de mvt souhaiter +2
-
-     îf(xposj(x)=xposj(y) &&(yposj(x)=yposj(y)-1||yposj(x)=yposj(y)+1)
-     // axe de mvt souhaiter +2
-
+#include "options.h"
 #include "passer au dessus du pion.h"
+int au_dessus_du_pion() {
+    int positionx1,positionx2,positiony1,positiony2;
+    if((positionx1=positionx2) && (positiony1=positiony2)) {
+        positionx1 = positionx1 +2;
+    }
+    else {
+        options();
+    }
+    return 0;
+}
