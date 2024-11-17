f8e66c8ae09193a91a373562f6fb6c380c3f47a5
hortense2006 <hortense.galtier@edu.ece.fr>
2024-11-17 12:43:49 +0100

    d

--- blocage d'un pion.c
+++ blocage d'un pion.c
@@ -2,12 +2,66 @@
 // Created by galti on 16/11/2024.
 //
 #include <stdio.h>
+#include <options.h>
 int bloque(){
+  int cote, compteur = 0;
     /* VERIFIER QUE LA BARRIERE NE BLOQUE PAS LE PION ADVERSE*/
     // le programme doit regarder chacun de cotes du pion
-    // s'il y a deja 3 barrieres encerclant le pion, le joueur ne peut pas poser de nouvelle barrieres.
-    if (barrieres == '\n'){
+  switch (cote){
+    case 1:{
+      cote = devant
+      if (cote != '\n'){
+        printf("Le cote est deja occupe.\n");
+        compteur = compteur + 1;
+      }
+      else {
+        printf("Le cote n'est pas occupe.\n");
+        compteur = compteur;
+      }
+      break;
+    }
+      case 2:{
+        cote = derriere
+        if (cote != '\n'){
+          printf("Le cote est deja occupe.\n");
+          compteur = compteur + 1;
+        }
+        else{
+          printf("Le cote n'est pas occupe.\n");
+        }
+        break;
+      }
+        case 3:{
+          cote = gauche
+          if (cote != '\n'){
+            printf("Le cote est deja occupe.\n");
+            compteur = compteur + 1;
+          }
+          else{
+            printf("Le cote n'est pas occupe.\n");
+            compteur = compteur;
+          }
+          break;
+        }
+          case 4:{
+            cote = droite
+            if (cote != '\n'){
+              printf("Le cote est deja occupe.\n");
+              compteur = compteur + 1;
+            }
+            else {
+              printf("Le cote n'est pas occupe.\n");
+              compteur = compteur;
+            }
+            break;
+          }
 
+    if (compteur != 3){
+      printf("Il y a moins de 3 barrieres, le choix est donc valide.\n");
+    }
+    else{
+      printf("Il y a deja 3 barrieres. Votre choix n'est donc pas possible.\n");
+      options();
     }
 }
 #include "blocage d'un pion.h"
