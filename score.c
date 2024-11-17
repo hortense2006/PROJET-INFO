9466e7013c4fb45fa4f6d7da378d56c2d846a080
Reage-X <sergekshyvnyak@gmail>
2024-11-17 16:57:34 +0100

    test

--- score.c
+++ score.c
@@ -1,36 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-
-int S=0; +=0; R=0
-int x = 3, y = 11
-int score(int S,+,R,*y)
-    {
+int score(int S)
+{
     if(S==1)
     {
-        int nx = 0, ny = 0;
-        if(+==1)
+        int y = 25, ny = 0;
+        char mT[3][1];
+        int mTop[1][25];
+        char mP[1][25];
+        int mS[1][25];
+        mT[0][0] = "Top     ";
+        mT[1][0] = "Player  ";
+        mT[2][0] = "Score   ";
+        for (ny = 0; ny <= y; ny++)
         {
-            &y= &y +10;
+            mTop[0][ny] = ny +1; //appelle top
+            mP[1][ny] = "Pseudo"; //appelle pseudo
+            mS[1][ny] =      0; //appelle score
         }
-        int char monTableau[x][&y];
-        for (ny = 1; ny < &y; ny++)
-        {
-            monTableau[0][0] = "Top"
-            monTableau[1][0] = "Player"
-            monTableau[2][0] = "Score"
-            nx = 0;
-            //
-            //  appelle pseudo
-            //
-            nx++;
-            //
-            //  appelle score
-            //
-            nx = 0;
-        }
-    }
-    if(R==1)
-    {
-        &y=10
     }
 }
