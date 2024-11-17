d943cf7ebec5af84a233c0cedc65b669c26b1ed0
hortense2006 <hortense.galtier@edu.ece.fr>
2024-11-17 12:53:56 +0100

    d

--- aide.c
+++ aide.c
@@ -3,17 +3,17 @@
 //
 #include <stdio.h>
 int aide(){
-  printf("Les règles du jeu sont :\n");
+  printf("Les regles du jeu sont :\n");
   printf("\n");
-  printf(" Regle 1 : Votre but est d'atteindre le bord opposé du plateau.\n");
+  printf(" Regle 1 : Votre but est d'atteindre le bord oppose du plateau.\n");
   printf("\n");
-  printf("Regle 2 : Vous avez 5 à 10 barrières à votre disposition, pour bloquer votre adversaire.\n");
+  printf("Regle 2 : Vous avez 5 à 10 barrieres a votre disposition, pour bloquer votre adversaire.\n");
   printf("\n");
   printf("Regle 3: Vous ne pouvez pas superposer 2 barrieres.\n");
   printf("\n");
   printf("Regle 4: Si votre pion se retrouve face à face avec un autre pion, vous pouvez passer au dessus de celui-ci.\n");
   printf("\n");
-  printf("Regle 4: Si votre partie est a 4 joueurs, vous ne pouvez pas sauter plus d'un pion à la fois.\n");
+  printf("Regle 4: Si votre partie est a 4 joueurs, vous ne pouvez pas sauter plus d'un pion a la fois.\n");
   printf("\n");
   printf("Regle 5: Vous ne pouvez annuler votre tour precedent qu'une seule fois\n");
   printf("\n");
