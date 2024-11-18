746f86c1e13ae05680f5322ce310dd30d89fcf57
AymericG <aymericgeron@yahoo.fr>
2024-11-18 14:59:03 +0100

    Commit message

--- ecriture_score.c
+++ ecriture_score.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
-void sauvegarde_score(char* pseudo, int score){
+void ecriture_score(char* pseudo, int score){
     FILE* pFichier = fopen(NULL"./sauvegarde_du_score.txt", "w"); // Ouverture du fichier sauvegarde_du_score.txt en mode écriture
     if (pFichier == NULL) { // Erreur d'ouverture de fichier
-      return 1;
+        return 1;
     }
     fclose(pFichier);
     pFichier = NULL;
--- sauvegarde_score.h
+++ sauvegarde_score.h
@@ -1,6 +1,6 @@
 #ifndef SAUVEGARDE_SCORE_H
 #define SAUVEGARDE_SCORE_H
 
-void sauvegarde_score();
+void ecriture_score();
 
 #endif //SAUVEGARDE_SCORE_H
