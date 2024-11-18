7725dde0a05a68d5d256a8d16bcb313b5a9579ef
hortense2006 <hortense.galtier@edu.ece.fr>
2024-11-18 18:20:27 +0100

    d

--- annuler son tour.c
+++ annuler son tour.c
@@ -8,7 +8,7 @@
 #define MAX 4
 int annuler(){
   // DECLARATION DES VARIABLES
-  int tour_du_joueur = 0, tour_precedent = 0, chiffrei, chiffreia;
+  int tour_du_joueur = 0, tour_precedent = 0, chiffrei, chiffreia, compteur = 1;
 
   printf("Voulez-vous annuler votre tour precedent?\n");
   printf("1. Oui.                     2 = Non.\n");
@@ -17,17 +17,18 @@ int annuler(){
   switch (chiffrei){
     case 1: {
       // ON VERIFIE QUE LE JOUEUR N'A PAS DEJA ANNULE UN TOUR
-      if (tour_du_joueur != tour_precedent ){
+      if (compteur!= 0){
         printf("Le joueur n'a pas encore annule un tour.\n");
         printf("Voulez-vous annuler votre tour precedent?\n");
         printf("1. Oui.                   2 = Non.\n");
-        printf("Saisissez un nombre ?\n");
+        printf("Saisissez soit 1 soit 2:\n");
         scanf("%d",&chiffreia);
         switch (chiffreia){
           case 1:{
             // LE JOUEUR DECIDE D'ANNULER SON TOUR
             if (tour_du_joueur == '\0'){
               tour_du_joueur = tour_precedent;
+              compteur = compteur -1;
             }
             else{
               printf("Le joueur a decide de jouer.\n");
