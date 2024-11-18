a2d76b3688bfae8aaf69466360376d3c7fd96890
AymericG <aymericgeron@yahoo.fr>
2024-11-18 14:41:58 +0100

    Commit message

---
+++ sauvegarde_du_score.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void sauvegarde_score(){
+    FILE* pFichier = fopen(NULL"./sauvegarde_du_score.txt", "w"); // Ouverture du fichier sauvegarde_du_score.txt en mode écriture
+    if (pFichier == NULL) { // Erreur d'ouverture de fichier
+      return 1;
+    }
+    fclose(pFichier);
+    pFichier = NULL;
+}
---
+++ sauvegarde_score.h
@@ -0,0 +1,6 @@
+#ifndef SAUVEGARDE_SCORE_H
+#define SAUVEGARDE_SCORE_H
+
+void sauvegarde_du_score();
+
+#endif //SAUVEGARDE_SCORE_H
