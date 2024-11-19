f28ee04375c3643bf9603d98c6dc42d72e318920
hortense2006 <hortense.galtier@edu.ece.fr>
2024-11-19 22:42:46 +0100

    d

--- table ascii.c
+++ table ascii.c
@@ -1,7 +1,21 @@
 //
 // Created by galti on 19/11/2024.
 //
+#include <stdio.h>
 int table_ascii(){
+    int i,j;
+    char tableau_ascii[i][j];
+    printf("Voici le tableau des caracteres disponibles:\n");
+    printf("%c",tableau_ascii[i][j]);
+
+    // Boucle à travers les valeurs ASCII de 0 à 127
+    for (int i = 0; i < 15 ; i++) {
+        // Affichage des valeurs décimales, hexadécimales, et des caractères
+        for (j = 0, j < 240, j+= 16) {
+            // Pour les caractères de contrôle, afficher un symbole (par exemple, .)
+            printf("%d\t%02X\t.\n", i, i);
+        }
+    }
 
 
 
