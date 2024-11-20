07e3aefffe109f7d740ca0fc64d5e57b86acaa34
hortense2006 <hortense.galtier@edu.ece.fr>
2024-11-20 17:22:45 +0100

    d

--- annuler son tour.c
+++ annuler son tour.c
@@ -6,33 +6,37 @@
 #include "annuler son tour.h"
 #define MIN 2
 #define MAX 4
-int annuler(){
+int annuler() {
   // DECLARATION DES VARIABLES
-  int tour_du_joueur = 0, tour_precedent = 0, chiffrei, compteur = 1;
+  int tour_du_joueur, tour_precedent, chiffrei, compteur = 1;
+
   printf("Voulez-vous annuler votre tour precedent?\n");
   printf("1. Oui.                     2 = Non.\n");
   printf("Saisissez un nombre.\n");
   scanf("%d",&chiffrei);
-  if(chiffrei == 1){
-    if (compteur!= 0){
-      // ON VERIFIE QUE LE JOUEUR N'A PAS DEJA ANNULE UN TOUR
+  if(chiffrei == 1) {
+    // ON VERIFIE QUE LE JOUEUR N'A PAS DEJA ANNULE UN TOUR
+    if (compteur!= 0) {
       printf("Le joueur n'a pas encore annule un tour.\n");
       printf("Voulez-vous annuler votre tour precedent?\n");
       printf("1. Oui.                   2 = Non.\n");
       printf("Saisissez soit 1 soit 2:\n");
       scanf("%d",&chiffrei);
-      // LE JOUEUR DECIDE D'ANNULER SON TOUR
-      if (chiffrei == 1){
+      if(chiffrei == 1) {
         tour_du_joueur = tour_precedent;
         compteur = compteur - 1;
       }
-      else{
-        printf("Erreur!\n");
+      else {
+        options();
       }
     }
+    else {
+      printf("Le joueur a deja annule un tour.\n");
+      options();
+    }
   }
-  else{
-    printf("Le joueur a deja annule un tour.\n");
+  else {
+    options();
   }
   return 0;
 }
