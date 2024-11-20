567671cf38ac2e3429469203cd8b1bba23a18771
AymericG <aymericgeron@yahoo.fr>
2024-11-20 13:01:52 +0100

    Commit message

--- signin.c
+++ signin.c
@@ -13,28 +13,31 @@ int signin()
     // PSEUDO
     printf("Choisissez un pseudo:\n");
     scanf("%c", &user);
-    if (user !== '\0' && // !==nom deja utilise){
-        printf("Votre nom d'utilisateur a ete enregistre.\n");
-        printf("Choisissez a present un mot de passe.\n");
-        scanf("%c", &mdp);
-          if (mdp < 12 && mdp > 12)  {
-                  printf("Votre mot de passe n'as pas 12 caracteres.");
-             }
-             else {
-                 printf("Votre mot de passe a été enregistré.");
-             }
-        } // on vérifie si le pseudo existe déjà.
-    else {
+    if (user < 20 || user > 20){
+      printf("Votre pseudo n'a pas le nombre de caracteres requis.\n");
+    }
+    else{
+        if (user !== '\0' && // !==nom deja utilise){
+         printf("Votre nom d'utilisateur a ete enregistre.\n");
+         printf("Choisissez a present un mot de passe.\n");
+         scanf("%c", &mdp);
+           if (mdp < 12 && mdp > 12)  {
+                   printf("Votre mot de passe n'as pas 12 caracteres.");
+              }
+           else {
+               printf("Votre mot de passe a été enregistré.");
+         }
+          // on vérifie si le pseudo existe déjà.
+        else {
         printf("Ce nom existe deja. Est-ce vous ?\n");
         printf("1 = Oui.                 2 = Non.\n");
         printf("Selectionner soit 1 soit 2:\n");
         scanf("%d", &chiffreb);
-            if (chiffreb == 1){
-              login();
-            }
-            else{
-              printf("Vous devez saisir un nouveau nom d'utilisateur.\n");
-            }
+        if (chiffreb == 1){
+            login();
+        }
+        else{
+            printf("Vous devez saisir un nouveau nom d'utilisateur.\n");
         }
     }
  return 0;
