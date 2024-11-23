15204671e310b5a24eef7713f5d8d679dcbf8b35
hortense2006 <hortense.galtier@edu.ece.fr>
2024-11-23 18:06:38 +0100

    d

--- options.c
+++ options.c
@@ -10,7 +10,7 @@
 int  options()
 {
     // DEFINITION DES VARIABLES
-    int options, pion = 1, barrieres = 2, passer = 3, annuler = 4, chiffre1;
+    int pion = 1, barrieres = 2, passer = 3, annuler = 4, chiffre1;
 
     // PRESENTATION DES OPTIONS DE JEU
     printf("Voici vos options de jeu :\n");
