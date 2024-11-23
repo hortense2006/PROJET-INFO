098943143bb077bfc0aae988c94ea9381f6e63c6
hortense2006 <hortense.galtier@edu.ece.fr>
2024-11-23 18:07:52 +0100

    d

--- reprendre une partie.c
+++ reprendre une partie.c
@@ -4,10 +4,12 @@
 #include <stdio.h>
 #include "reprendre une partie.h"
 #include "lecture_plateau.h"
+#include "tour_du_joueur.h"
 int reprendre_une_partie(){
 // APPEL PLATEAU
 lecture_plateau();
 // APPEL PLACEMENTS DES BARRIERES DANS LA BASE DE DONNEES
 // APPEL DE L'ORDRE DES JOUEURS + QUI DOIT JOUER
+tour();
 }
 
