ec90b4eb53bc68f262335170bad6de191442c693
Reage-X <sergekshyvnyak@gmail>
2024-11-24 21:54:48 +0100

    test

--- augmentation_score.h
+++ augmentation_score.h
@@ -1,6 +1,4 @@
 #ifndef AUGMENTATION_SCORE_H
 #define AUGMENTATION_SCORE_H
-
 void augmentation_score(char pseudo[], int augmentation);
-
 #endif //AUGMENTATION_SCORE_H
--- lecture_score.h
+++ lecture_score.h
@@ -1,6 +1,4 @@
 #ifndef LECTURE_SCORE_H
 #define LECTURE_SCORE_H
-
 int lecture_score(char* pseudo);
-
 #endif //LECTURE_SCORE_H
