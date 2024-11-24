22546d23290751dc286da546ee9e029a1c741d48
Reage-X <sergekshyvnyak@gmail>
2024-11-25 00:03:22 +0100

    test

---
+++ Score.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sauvegarde_du_pseudo.h"
+int score(int S)
+// S = BOUTON ON/OFF
+{
+    if(S==1) // Le bouton ON/OFF est activé.
+    {
+        int y = 25, ny = 0;
+        char mT[3][1];
+        int mTop[1][25];
+        char mP[1][25];
+        int mS[1][25];
+        mT[0][0] = "Top";
+        mT[1][0] = "Player";
+        mT[2][0] = "Score";
+        for (ny = 0; ny <= y; ny++)
+        {
+            mTop[0][ny] = ny +1;
+            mP[1][ny] = "Pseudo"; //appelle pseudo
+            ecriture_pseudo(); // le pseudo est extrait de la base de données
+            mS[1][ny] = 0; //appelle score
+            score();
+
+        }
+    }
+}
---
+++ Score.h
@@ -0,0 +1,4 @@
+#ifndef SCORE_H
+#define SCORE_H
+int score();
+#endif //SCORE_H
