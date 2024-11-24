bb76a2fb0f5867dda8110e9526adb6690c7151c9
hortense2006 <hortense.galtier@edu.ece.fr>
2024-11-24 16:50:39 +0100

    d

--- tour des joueurs.c
+++ tour des joueurs.c
@@ -5,16 +5,39 @@
 #include "déplacement du pion.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int tour(Partie -> nbjoueurs, Partie -> ordretour){
   // ORDRE ALEATOIRE DES JOUEURS
-  int joueur = 0, var3, var, var1, var2;
+  int joueur = 0, var3, var, var1, var2, tableau[4], i;
+  // FONCTION DE COMPARAISON POUR QSORT
 
-  scanf(rand(),var);
-  scanf(rand(),var1);
-  scanf(rand(),var2);
-  scanf(rand(),var3);
+  int comparer (const void *a, const void *b);
+  {
+    return (*(int*)a - *(int*)b);
+  }
 
-  return ;
+  // ON INITIALISE LE GENERATEUR DE VARIABLES ALEATOIRES
+  srand(time(NULL));
+
+  // ON GENERE LES 4 VARIABLES
+  for (i = 0; i < 4; var++){
+    tableau[i] = rand() % 4;
+  }
+  // ON LES AFFICHE DANS UN TABLEAU
+  for (int i = 0; i < 4; i++){
+    printf("%d ", tableau[i]);
+  }
+
+  // TRI DU TABLEAU
+  qsort(tableau,4,sizeof(int), comparer);
+
+  // AFFICHAGE DES VARIABLES TRIEES
+  printf("L'ordre des joueurs est :\n");
+  for(i = 0; i < 4; var++){
+    printf("%d ",tableau[i]);
+  }
+
+  return 0;
 }
 
