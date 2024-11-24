db2f27c38e82bb9e6b0e886312da629988b988e6
hortense2006 <hortense.galtier@edu.ece.fr>
2024-11-24 22:02:21 +0100

    d

--- nouvelle partie.c
+++ nouvelle partie.c
@@ -4,15 +4,11 @@
 #include <stdio.h>
 #include "nouvelle partie.h"
 #include "affichagePlateau.h"
-#include "positionnement d'une barrière.h"
 int nouvelle_partie()
 {
   // APPEL PLATEAU
     affichagePlateau(Plateau *plateau, int playerX, int playerY)
-  // APPEL POSITION INITIALE DU PION
-  pion();
-  // APPEL BARRIERES
-    barrieres();
+
   return 0;
   }
 
