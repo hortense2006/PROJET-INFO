8aa6e63e6320482fca01272698bf7fdd8f04a7fd
hortense2006 <hortense.galtier@edu.ece.fr>
2024-11-25 00:17:50 +0100

    d

--- accueil.c
+++ accueil.c
@@ -3,3 +3,60 @@
 //
 
 #include "accueil.h"
+#include <stdio.h>
+#include "signin.h"
+#include "login.h"
+#include "accueil.h"
+int accueil()
+{
+    // DEFINITION DES VARIABLES
+    int chiffrea;
+
+    printf("Bienvenue dans le jeu Quoridor!\n");
+
+    // OPTIONS DE CONNEXION
+    printf(" 1 = Log in.                   2 = Sign in\n");
+    printf("Selectionner soit 1 soit 2:\n");
+    scanf("%d",&chiffrea);
+    switch (chiffrea) {
+        case 1: {
+            printf("Voulez vous vous connecter ?\n");
+            printf("1 = Oui.                   2 = Non.\n");
+            printf("Selectionner soit 1 soit 2:\n");
+            scanf("%d",&chiffrea);
+            if (chiffrea == 1) {
+              login();
+            }
+        break;
+        }
+        case 2: {
+            printf("Voulez-vous créer un compte ?\n");
+            printf("1 = Oui.                       2 = Non.\n");
+            printf("Selectionner soit 1 soit 2:\n");
+            scanf("%d",&chiffrea);
+            if (chiffrea == 1) {
+              signin();
+            }
+        default :{
+            printf("Erreur\n");
+        }
+    }
+      // on définit une structure pour chaque joueur, afin d'associer au joueur toutes les variables le concernant.
+      struct joueur1{
+        int user;
+        int mdp;
+      }
+      struct joueur2{
+        int user;
+        int mdp;
+      }
+      struct joueur3{
+       int user;
+       int mdp;
+      }
+      struct joueur4{
+        int user;
+        int mdp;
+      }
+return 0;
+}
