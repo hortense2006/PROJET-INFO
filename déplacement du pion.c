894921e67e187215d058a497ebc95a470359c25c
hortense2006 <hortense.galtier@edu.ece.fr>
2024-11-24 17:07:16 +0100

    d

--- déplacement du pion.c
+++ déplacement du pion.c
@@ -7,28 +7,27 @@
 
 int pion() {
     // DEFINITION DES VARIABLES
-    int chiffre2,position_x,position_y, i, j;
+    int x,y, i, j, D;
   /*VISUEL DES PIONS*/
   printf("Entrez les coordonnees du pion de votre choix:\n");
   scanf("%d""%d",&i,&j);
 
   /* PRESENTATION DES OPTIONS*/
-
+  printf("Indiquez la direction D du placement de votre pion:\n");
   printf("1 = Aller vers la gauche.           2. Aller vers la droite.\n");
   printf("3 = Avancer d'une case.              4 = Reculer d'une case.\n");
-  printf("Indiquez la direction D du placement de votre pion:\n");
   scanf("%d",&D);
   void placementPion();
 
   /* VERIFIER SI LE MOUVEMENT EST POSSIBLE*/
-  if (position_x !='\0') {
+  if (x !='\0') {
     printf("Cette case est déjà occupee.\n");
-    if (position_y + 1 !='\0'|| position_y - 1 !='\0') {
+    if (y + 1 !='\0'|| y - 1 !='\0') {
       printf("Cette case est occupee par une barriere.\n");
     }
     else {
       // PASSER AU DESSUS DU PION
-      position_x = position_x + 2;
+      x = x + 2;
     }
   }
   else{
