3421edf0bbdaa9d4f38889ed5c83447913975b5a
hortense2006 <hortense.galtier@edu.ece.fr>
2024-11-24 22:05:20 +0100

    d

--- reprendre une partie.c
+++ reprendre une partie.c
@@ -3,11 +3,11 @@
 //
 #include <stdio.h>
 #include "reprendre une partie.h"
-#include "lecture_plateau.h"
+#include "ecriture_plateau.h"
 #include "tour des joueurs.h"
 int reprendre_une_partie(){
 // APPEL PLATEAU
-lecture_plateau();
+ecriture_plateau(char plateau[17][17])
 
 // APPEL DE L'ORDRE DES JOUEURS + QUI DOIT JOUER
 tour(Partie -> nbjoueurs, Partie -> ordretour, int nbj);
