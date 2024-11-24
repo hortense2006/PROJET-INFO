f3ff970e5bca1be5214baf397e81d0ae12cd7207
hortense2006 <hortense.galtier@edu.ece.fr>
2024-11-25 00:16:59 +0100

    d

---
+++ accueil.c
@@ -0,0 +1,5 @@
+//
+// Created by galti on 25/11/2024.
+//
+
+#include "accueil.h"
---
+++ accueil.h
@@ -0,0 +1,8 @@
+//
+// Created by galti on 25/11/2024.
+//
+
+#ifndef ACCUEIL_H
+#define ACCUEIL_H
+
+#endif //ACCUEIL_H
